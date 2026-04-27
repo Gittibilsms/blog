f97d74e9208bee4251ff51b42faf3537defa2066
mailguven <mailguvenpanel@gmail.com>
2026-04-27 21:41:58 +0400

    website pricing updated

---
+++ Blog/Models/Pricing.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog.Models
+{
+    public class Pricing
+    {
+        public int PricingId { get; set; }
+        public string Currency { get; set; }
+        public decimal Low { get; set; }
+        public decimal Middle { get; set; }
+        public decimal High { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
